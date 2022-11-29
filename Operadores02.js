af87b078b7dfd88fa1d94261d986244a86054d14
David Marins <davidmarinsdiogo01@gmail.com>
2022-11-29 08:50:14 -0300

    Operadores

---
+++ Operadores02.js
@@ -0,0 +1,17 @@
+console.log("Se eu bebo água e suco, Minha sede termina.")
+
+if("Se eu bebo água" && "suco"){
+    let saida = "Minha sede termina."
+    console.log(saida) 
+}
+console.log("Se caminho 5 km e corro 2 km, passo o dia bem.")
+if("Se caminho 5 km " && "corro 2 km"){
+    let dia = "Passo o dia bem!"
+    console.log(dia)
+}
+console.log("Se levando cedo ou antes das 8h, saio tranquilo")
+if("Se levando cedo" || "antes das 8h"){
+    let cedo ="Saio tranquilo."
+    console.log(cedo)
+}
+
---
+++ operadores03.js
@@ -0,0 +1,9 @@
+if ("caminho 5 km" && "corro 2km") {
+    condole.log("Passo dia bem.")
+}
+if ("levanto cedo" || "antes das 8h") {
+    console.log("saio tranquilo")
+}
+if ("Não termino o trabalho" || "Não tenho compromisso") {
+    console.log("Saio tarde.")
+}
